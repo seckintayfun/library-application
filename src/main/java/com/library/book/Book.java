a3485c0ffe2704d1094c86af1237617b57aafbf8
seckintayfun <seckintayfun.caglayan@gmail.com>
2023-12-11 01:00:00 +0300

    user and book entities added

---
+++ src/main/java/com/library/book/Book.java
@@ -0,0 +1,30 @@
+package com.library.book;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "genre", nullable = false)
+    private String genre;
+
+    @Column(name = "publisher", nullable = false)
+    private String publisher;
+}
---
+++ src/main/java/com/library/user/User.java
@@ -0,0 +1,35 @@
+package com.library.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "member_since", nullable = false)
+    private Date memberSince;
+
+    @Column(name = "member_till")
+    private Date memberTill;
+
+    @Column(name = "gender", nullable = false)
+    private Character gender;
+}
